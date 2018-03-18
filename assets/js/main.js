7b9820c45c6ece60121da764faa29d59846dee91
Tessa <Tessajaworski@gmail.com>
2018-03-18 22:02:52 +0100

    Change main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,59 +15,55 @@ function startTime() {
     var year = today.getFullYear(); // variable voor de jaar 
     minutes = checkTime(minutes);
     seconds = checkTime(seconds);
-    document.getElementById('mineClock').innerHTML =
-    hours + ":" + minutes + ":" ; // uren en minuten
-    document.getElementById('sec').innerHTML = 
-    seconds ; // seconden
+    document.getElementById('mineClock').innerHTML = hours + ":" + minutes + ":" ; // uren en minuten
+    document.getElementById('sec').innerHTML = seconds ; // seconden
     var time = setTimeout(startTime, 1000); // tijd per seconden
 
- 
-
-
+    // de dag maand en jaar toont hij op mijn scherm van id dayMonthYear
     document.getElementById('dayMonthYear').innerHTML = day + " " + dayNumber + " " + month + " " + year; // dag, dag in getal, maand en jaar
         
         // if hours om verschillende tijden met verschillende afbeeldingen en elementen te laten zien
-        if (hours >= 2 && hours <= 6) {
+        // is het 2 uur of hoger laat dit zien // is het 6 uur of lager dit zien
+    if (hours >= 2 && hours <= 6) {
         
-            document.body.background = "assets/img/night.jpg";
-            document.getElementById("greeting").innerHTML = "Good night,";
-            document.getElementById("sun").style.display = "none";
-            document.getElementById("moon").style.display = "initial";
-            document.getElementById("mineClock").style.color = "#fff";
-            document.getElementById("sec").style.color = "#fff";
-            document.getElementById("dayMonthYear").style.color = "#fff";
-            document.getElementById("greeting").style.color = "#fff";
+        document.body.background = "assets/img/night.jpg";
+        document.getElementById("greeting").innerHTML = "Good night,";
+        document.getElementById("sun").style.display = "none";
+        document.getElementById("moon").style.display = "initial";
+        document.getElementById("mineClock").style.color = "#fff";
+        document.getElementById("sec").style.color = "#fff";
+        document.getElementById("dayMonthYear").style.color = "#fff";
+        document.getElementById("greeting").style.color = "#fff";
             
         
     }
-        if ( hours >= 7 && hours <= 12) {
-        
-            document.body.background = "assets/img/morning3.jpg";
-            document.getElementById("greeting").innerHTML = "Good morning,";
-            document.getElementById("mineClock").style.color = "#745848";
-            document.getElementById("sec").style.color = "#745848";
-            document.getElementById("dayMonthYear").style.color = "#745848";
-            document.getElementById("greeting").style.color = "#745848";
-            document.getElementById("sun").style.display = "initial";
-            document.getElementById("moon").style.display = "none";
+        // is het 7.00 of hoger laat dit zien // is het 12 uur of lager laat dit zien
+    if ( hours >= 7 && hours <= 12) {
+    
+        document.body.background = "assets/img/morning3.jpg";
+        document.getElementById("greeting").innerHTML = "Good morning,";
+        document.getElementById("mineClock").style.color = "#745848";
+        document.getElementById("sec").style.color = "#745848";
+        document.getElementById("dayMonthYear").style.color = "#745848";
+        document.getElementById("greeting").style.color = "#745848";
+        document.getElementById("sun").style.display = "initial";
+        document.getElementById("moon").style.display = "none";
             
         
     }
-
-        if (hours >= 13 && hours <= 17) {
+            // als het 13.00 uur is of hoger laat dit zien // is het 17.00 of lager laat dit zien
+    if (hours >= 13 && hours <= 17) {
         
-            document.body.background = "assets/img/day2.jpg";
-            document.getElementById("greeting").innerHTML = "Good day,";
-            document.getElementById("sun").style.display = "initial";
-            document.getElementById("moon").style.display = "none";
-            document.getElementById("mineClock").style.color = "#fff";
-            document.getElementById("sec").style.color = "#fff";
-            document.getElementById("dayMonthYear").style.color = "#fff";
-            document.getElementById("greeting").style.color = "#fff";
-            
-        
+        document.body.background = "assets/img/day2.jpg";
+        document.getElementById("greeting").innerHTML = "Good day,";
+        document.getElementById("sun").style.display = "initial";
+        document.getElementById("moon").style.display = "none";
+        document.getElementById("mineClock").style.color = "#fff";
+        document.getElementById("sec").style.color = "#fff";
+        document.getElementById("dayMonthYear").style.color = "#fff";
+        document.getElementById("greeting").style.color = "#fff";     
     }
-
+    // is het 18.00 of 0.00 of hoger laat dit zien // is het 23 uur of 1.00 of lager laat dit zien
     if (hours >= 18 && hours <= 23 || hours >= 0 && hours <= 1) {
         document.body.background = "assets/img/evening.jpg";
         document.getElementById("greeting").innerHTML = "Good afternoon,"; // greeting
@@ -105,6 +101,7 @@ TweenMax.to (sun, 50, {
     ease: Linear.easeInOut,
 });
 
+//sun opacity animatie
 var sunOpacity = document.getElementById('sun');
 TweenMax.to (sun, 1, {
     opacity: 0.3,
@@ -125,6 +122,7 @@ TweenMax.to (moon, 50, {
     ease: Linear.easeInOut,
 });
 
+//tweenmax moon opacity animatie
 var moonOpacity = document.getElementById('moon');
 TweenMax.to (moon, 1, {
     opacity: 0.3,
@@ -133,7 +131,7 @@ TweenMax.to (moon, 1, {
     yoyo: true,
 
 });
-
+// tweenmax logo opacity effect animatie
 var logo = document.getElementById('logo');
 TweenMax.to (logo, 5, {
     opacity: 0.3,
